f48696b5da44ca4cf79f6eaac654e49e8fecf1bd
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-01 17:39:27 +0700

    Database Connection

---
+++ app/Config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Config;
+
+class Database
+{
+    private static ?\PDO $pdo = null;
+
+    public static function getConnection(string $env = "test"): \PDO{
+        if(self::$pdo == null){
+            // create new PDO
+            require_once __DIR__ . '/../../config/database.php';
+            $config = getDatabaseConfig();
+            self::$pdo = new \PDO(
+                $config['database'][$env]['url'],
+                $config['database'][$env]['username'],
+                $config['database'][$env]['passwor']
+            );
+        }
+
+        return self::$pdo;
+    }
+}
---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+function getDatabaseConfig(): array
+{
+    return [
+        "database" => [
+            "test" => [
+                "url" => "mysql:host=localhost:3306;dbname=php_user_management_test",
+                "username" => "root",
+                "password" => ""
+            ],
+            "prod" => [
+                "url" => "mysql:host=localhost:3306;dbname=php_user_management",
+                "username" => "root",
+                "password" => ""
+            ]
+        ]
+    ];
+}
