a33b1cc7d0714971a0fb97b2110f998a20793aaa
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-03 12:30:38 +0700

    Unit Test Session Repository

---
+++ test/Repository/SessionRepositoryTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Repository;
+
+use PHPUnit\Framework\TestCase;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\Session;
+
+class SessionRepositoryTest extends TestCase
+{
+    private SessionRepository $sessionRepository;
+
+    protected function setUp():void
+    {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+
+        $this->sessionRepository->deleteAll();
+    }
+
+    public function testSaveSuccess()
+    {
+        $session = new Session();
+        $session->id = uniqid();
+        $session->userId = "eko";
+
+        $this->sessionRepository->save($session);
+
+        $result = $this->sessionRepository->findById($session->id);
+        self::assertEquals($session->id, $result->id);
+        self::assertEquals($session->userId, $result->userId);
+    }
+
+    public function testDeleteByIdSuccess()
+    {
+        $session = new Session();
+        $session->id = uniqid();
+        $session->userId = "eko";
+
+        $this->sessionRepository->save($session);
+
+        $result = $this->sessionRepository->findById($session->id);
+        self::assertEquals($session->id, $result->id);
+        self::assertEquals($session->userId, $result->userId);
+
+        $this->sessionRepository->deleteById($session->id);
+
+        $result = $this->sessionRepository->findById($session->id);
+        self::assertNull($result);
+    }
+
+    public function testFindByIdNotFound()
+    {
+        $result = $this->sessionRepository->findById('notfound');
+        self::assertNull($result);
+    }
+
+
+}
