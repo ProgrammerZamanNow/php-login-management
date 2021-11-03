5b4bfdf9c6280bd4f5a24a0e83f3bbaa070d1a8a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-03 09:09:51 +0700

    Unit Test User Registration Service

---
+++ test/Service/UserServiceTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Service;
+
+use PHPUnit\Framework\TestCase;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Exception\ValidationException;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterRequest;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
+
+class UserServiceTest extends TestCase
+{
+    private UserService $userService;
+    private UserRepository $userRepository;
+
+    protected function setUp():void
+    {
+        $connection = Database::getConnection();
+        $this->userRepository = new UserRepository($connection);
+        $this->userService = new UserService($this->userRepository);
+
+        $this->userRepository->deleteAll();
+    }
+
+    public function testRegisterSuccess()
+    {
+        $request = new UserRegisterRequest();
+        $request->id = "eko";
+        $request->name = "Eko";
+        $request->password = "rahasia";
+
+        $response = $this->userService->register($request);
+
+        self::assertEquals($request->id, $response->user->id);
+        self::assertEquals($request->name, $response->user->name);
+        self::assertNotEquals($request->password, $response->user->password);
+
+        self::assertTrue(password_verify($request->password, $response->user->password));
+    }
+
+    public function testRegisterFailed()
+    {
+        $this->expectException(ValidationException::class);
+
+        $request = new UserRegisterRequest();
+        $request->id = "";
+        $request->name = "";
+        $request->password = "";
+
+        $this->userService->register($request);
+    }
+
+    public function testRegisterDuplicate()
+    {
+        $user = new User();
+        $user->id = "eko";
+        $user->name = "Eko";
+        $user->password = "rahasia";
+
+        $this->userRepository->save($user);
+
+        $this->expectException(ValidationException::class);
+
+        $request = new UserRegisterRequest();
+        $request->id = "eko";
+        $request->name = "Eko";
+        $request->password = "rahasia";
+
+        $this->userService->register($request);
+    }
+
+}
