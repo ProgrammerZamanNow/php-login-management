a2ac2538fe49a687b51f7ee1e845450b4be66089
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-03 14:33:45 +0700

    User Profile Update Repository

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -22,6 +22,15 @@ class UserRepository
         return $user;
     }
 
+    public function update(User $user): User
+    {
+        $statement = $this->connection->prepare("UPDATE users SET name = ?, password = ? WHERE id = ?");
+        $statement->execute([
+            $user->name, $user->password, $user->id
+        ]);
+        return $user;
+    }
+
     public function findById(string $id): ?User
     {
         $statement = $this->connection->prepare("SELECT id, name, password FROM users WHERE id = ?");
@@ -42,7 +51,8 @@ class UserRepository
         }
     }
 
-    public function deleteAll():void {
+    public function deleteAll(): void
+    {
         $this->connection->exec("DELETE from users");
     }
 }
