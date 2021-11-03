6401ec0f541eab8a8400b87cdb6f19bcd6a8751b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-03 13:51:17 +0700

    Session Middleware

---
+++ app/Middleware/MustNotLoginMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Middleware;
+
+use ProgrammerZamanNow\Belajar\PHP\MVC\App\View;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\SessionRepository;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Service\SessionService;
+
+class MustNotLoginMiddleware implements Middleware
+{
+    private SessionService $sessionService;
+
+    public function __construct()
+    {
+        $sessionRepository = new SessionRepository(Database::getConnection());
+        $userRepository = new UserRepository(Database::getConnection());
+        $this->sessionService = new SessionService($sessionRepository, $userRepository);
+    }
+
+    function before(): void
+    {
+        $user = $this->sessionService->current();
+        if ($user != null) {
+            View::redirect('/');
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\UserController;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\MustNotLoginMiddleware;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\MustLoginMiddleware;
 
 Database::getConnection('prod');
 
@@ -13,10 +15,10 @@ Database::getConnection('prod');
 Router::add('GET', '/', HomeController::class, 'index', []);
 
 // User Controller
-Router::add('GET', '/users/register', UserController::class, 'register', []);
-Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
-Router::add('GET', '/users/login', UserController::class, 'login', []);
-Router::add('POST', '/users/login', UserController::class, 'postLogin', []);
-Router::add('GET', '/users/logout', UserController::class, 'logout', []);
+Router::add('GET', '/users/register', UserController::class, 'register', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/login', UserController::class, 'login', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/login', UserController::class, 'postLogin', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/logout', UserController::class, 'logout', [MustLoginMiddleware::class]);
 
 Router::run();
