29b9a0ac0970825038e6cd6e31daea0e90e17b41
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-03 14:36:42 +0700

    Unit Test User Profile Update Repository

--- test/Repository/UserRepositoryTest.php
+++ test/Repository/UserRepositoryTest.php
@@ -10,9 +10,13 @@ class UserRepositoryTest extends TestCase
 {
 
     private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     protected function setUp(): void
     {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();
+
         $this->userRepository = new UserRepository(Database::getConnection());
         $this->userRepository->deleteAll();
     }
@@ -38,5 +42,25 @@ class UserRepositoryTest extends TestCase
         $user = $this->userRepository->findById("notfound");
         self::assertNull($user);
     }
+
+    public function testUpdate()
+    {
+        $user = new User();
+        $user->id = "eko";
+        $user->name = "Eko";
+        $user->password = "rahasia";
+
+        $this->userRepository->save($user);
+
+        $user->name = "Budi";
+        $this->userRepository->update($user);
+
+        $result = $this->userRepository->findById($user->id);
+
+        self::assertEquals($user->id, $result->id);
+        self::assertEquals($user->name, $result->name);
+        self::assertEquals($user->password, $result->password);
+    }
+
 
 }
