3a5d356b9949bbb50ba471ff98db44c04da67f9b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-03 11:54:18 +0700

    User Login Controller

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC\Controller;
 use ProgrammerZamanNow\Belajar\PHP\MVC\App\View;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Exception\ValidationException;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginRequest;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterRequest;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Service\UserService;
@@ -20,13 +21,15 @@ class UserController
         $this->userService = new UserService($userRepository);
     }
 
-    public function register(){
+    public function register()
+    {
         View::render('User/register', [
             'title' => 'Register new User'
         ]);
     }
 
-    public function postRegister(){
+    public function postRegister()
+    {
         $request = new UserRegisterRequest();
         $request->id = $_POST['id'];
         $request->name = $_POST['name'];
@@ -35,9 +38,33 @@ class UserController
         try {
             $this->userService->register($request);
             View::redirect('/users/login');
-        }catch (ValidationException $exception){
+        } catch (ValidationException $exception) {
             View::render('User/register', [
                 'title' => 'Register new User',
+                'error' => $exception->getMessage()
+            ]);
+        }
+    }
+
+    public function login()
+    {
+        View::render('User/login', [
+            "title" => "Login user"
+        ]);
+    }
+
+    public function postLogin()
+    {
+        $request = new UserLoginRequest();
+        $request->id = $_POST['id'];
+        $request->password = $_POST['password'];
+
+        try {
+            $this->userService->login($request);
+            View::redirect('/');
+        } catch (ValidationException $exception) {
+            View::render('User/login', [
+                'title' => 'Login user',
                 'error' => $exception->getMessage()
             ]);
         }
--- public/index.php
+++ public/index.php
@@ -15,5 +15,7 @@ Router::add('GET', '/', HomeController::class, 'index', []);
 // User Controller
 Router::add('GET', '/users/register', UserController::class, 'register', []);
 Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
+Router::add('GET', '/users/login', UserController::class, 'login', []);
+Router::add('POST', '/users/login', UserController::class, 'postLogin', []);
 
 Router::run();
