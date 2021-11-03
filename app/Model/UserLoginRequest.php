fe9d05f910dbeaf88c2a189a31ecb09cfc9e3739
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2021-11-03 11:27:54 +0700

    User Login Service

---
+++ app/Model/UserLoginRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Model;
+
+class UserLoginRequest
+{
+    public ?string $id = null;
+    public ?string $password = null;
+}
--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -5,6 +5,8 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC\Service;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Exception\ValidationException;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginRequest;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserLoginResponse;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterRequest;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Model\UserRegisterResponse;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Repository\UserRepository;
@@ -25,7 +27,7 @@ class UserService
         try {
             Database::beginTransaction();
             $user = $this->userRepository->findById($request->id);
-            if($user != null){
+            if ($user != null) {
                 throw new ValidationException("User Id already exists");
             }
 
@@ -41,7 +43,7 @@ class UserService
 
             Database::commitTransaction();
             return $response;
-        }catch (\Exception $exception){
+        } catch (\Exception $exception) {
             Database::rollbackTransaction();
             throw $exception;
         }
@@ -52,6 +54,32 @@ class UserService
         if ($request->id == null || $request->name == null || $request->password == null ||
             trim($request->id) == "" || trim($request->name) == "" || trim($request->password) == "") {
             throw new ValidationException("Id, Name, Password can not blank");
+        }
+    }
+
+    public function login(UserLoginRequest $request): UserLoginResponse
+    {
+        $this->validateUserLoginRequest($request);
+
+        $user = $this->userRepository->findById($request->id);
+        if ($user == null) {
+            throw new ValidationException("Id or password is wrong");
+        }
+
+        if (password_verify($request->password, $user->password)) {
+            $response = new UserLoginResponse();
+            $response->user = $user;
+            return $response;
+        } else {
+            throw new ValidationException("Id or password is wrong");
+        }
+    }
+
+    private function validateUserLoginRequest(UserLoginRequest $request)
+    {
+        if ($request->id == null || $request->password == null ||
+            trim($request->id) == "" || trim($request->password) == "") {
+            throw new ValidationException("Id, Password can not blank");
         }
     }
 }
